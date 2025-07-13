771d5ba2effc119818f56d6333bd620e65557f90
Paula Galdames <sitio.galdames@gmail.com>
2025-07-12 23:14:37 -0400

    establecer conección consulta SQL mostrar resultado de una tabla

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,56 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        Scanner sc = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        try {
+
+            // Establecer conexión
+            String url = "jdbc:mysql://localhost:3306/empresa";
+            String user = "root";
+            String password = "";
+
+            connection = DriverManager.getConnection(url, user, password);
+            System.out.println("Conexión exitosa.");
+
+            // Consulta SQL
+            String consulta = "SELECT * FROM empleado";
+            preparedStatement = connection.prepareStatement(consulta);
+            resultSet = preparedStatement.executeQuery();
+
+            // Mostrar resultados
+            while (resultSet.next()) {
+                int id = resultSet.getInt("EmpleadoID");
+                String nombre = resultSet.getString("Nombre");
+                String cargo = resultSet.getString("Cargo");
+                Float  salario = resultSet.getFloat("Salario");
+                System.out.println("ID: " + id + ", Nombre: " + nombre + ", Cargo: " + cargo + ", Salario: " + salario);
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            // Cierro conexión
+            try {
+                if (resultSet != null) resultSet.close();
+                if (preparedStatement != null) preparedStatement.close();
+                if (connection != null) connection.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
+
+        sc.close();
     }
 }
+
